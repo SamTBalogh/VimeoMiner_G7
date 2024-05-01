a3472736e95162247af86ad9af63ac0f2335d04b
Samuel Tamayo <samtambal@alum.us.es>
2024-05-01 14:07:02 +0200

    Service y test ampliados. Exceptions añadidas. Modelos modificados.

--- src/main/java/aiss/vimeominer/model/VimeoMiner/video/VimeoVideo.java
+++ src/main/java/aiss/vimeominer/model/VimeoMiner/video/VimeoVideo.java
@@ -4,8 +4,6 @@ package aiss.vimeominer.model.VimeoMiner.video;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.util.List;
-
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class VimeoVideo {
 
